d219bdb2a92467d677cc6ff271b18668e593669c
svandenhoven <svandenhoven@hotmail.com>
2016-06-08 08:47:04 +0200

    Changed architecture to SF Only

---
+++ RoadRunners/RoadRunners.CarActor.Interfaces/RoadRunners.Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadRunners.CarActor.Interfaces
+{
+    public enum CarStates
+    {
+        Unknown =0,
+        Start   =1,
+        End     =2
+    }
+}
